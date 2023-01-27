8c43deeee56820771d3ce53460ee971382862026
yodayeong <duekdud01@naver.com>
2023-01-27 17:07:55 +0900

    fighting!!!!!

--- 20230126/temp.py
+++ 20230126/temp.py
@@ -1,17 +1,3 @@
-def factorial(n):
-    num = 1
+temp = [1, 1, 2]
 
-    for i in range(1, n+1):
-        num *= i
-
-    return num
-
-print(factorial(3))
-
-def factorial_self(n):
-    if n == 1:
-        return 1
-    else:
-        return n * factorial_self(n-1)
-
-print(factorial_self(3))
+print(sum(temp))
